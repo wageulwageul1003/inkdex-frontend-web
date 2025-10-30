b60700632dc0c14a77d668980f979b3a9fcf8143
JungYeonHwi <wjddusgnl676@naver.com>
2025-10-31 02:14:18 +0900

    feat: add social login and UI-related icons to public assets

--- src/components/shared/icons.tsx
+++ src/components/shared/icons.tsx
@@ -1,3 +1,6 @@
+import Apple from '@/public/icons/Apple.svg';
+import Google from '@/public/icons/Google.svg';
+import ArrowBackIos from '@/public/icons/arrow_back_ios.svg';
 import arrowLeftAlt from '@/public/icons/arrow_left_alt.svg';
 import arrowRightAlt from '@/public/icons/arrow_right_alt.svg';
 import asterisk from '@/public/icons/asterisk.svg';
@@ -7,6 +10,7 @@ import checkBox from '@/public/icons/check_box.svg';
 import checkBoxIndeterminate from '@/public/icons/check_box_indeterminate.svg';
 import checkBoxOutlineBlank from '@/public/icons/check_box_outline_blank.svg';
 import checkCircleFill from '@/public/icons/check_circle_fill.svg';
+import kakao from '@/public/icons/kakao.svg';
 import closeSmall from '@/public/icons/close_small.svg';
 import dehaze from '@/public/icons/dehaze.svg';
 import emergencyHome from '@/public/icons/emergency_home.svg';
@@ -26,9 +30,14 @@ import schedule from '@/public/icons/schedule.svg';
 import search from '@/public/icons/search.svg';
 import visibility from '@/public/icons/visibility.svg';
 import visibilityOff from '@/public/icons/visibility_off.svg';
-import close from '@/public/icons/visibility_off.svg';
+import close from '@/public/icons/close_large.svg';
+import email from '@/public/icons/email.svg';
 
 export const Icons = {
+  Apple,
+  Google,
+  kakao,
+  ArrowBackIos,
   arrowLeftAlt,
   arrowRightAlt,
   asterisk,
@@ -58,4 +67,5 @@ export const Icons = {
   visibility,
   visibilityOff,
   close,
+  email,
 };
