a7c77d13f71c295b5e22cfb2e248bb31253f15cf
JungYeonHwi <wjddusgnl676@naver.com>
2025-11-02 23:23:39 +0900

    feat: add bell, camera, ellipse and photo icons to shared icon library

--- src/components/shared/icons.tsx
+++ src/components/shared/icons.tsx
@@ -2,7 +2,9 @@ import ArrowBackIos from '@/public/icons/arrow_back_ios.svg';
 import arrowLeftAlt from '@/public/icons/arrow_left_alt.svg';
 import arrowRightAlt from '@/public/icons/arrow_right_alt.svg';
 import asterisk from '@/public/icons/asterisk.svg';
+import bell from '@/public/icons/bell.svg';
 import calendarMonth from '@/public/icons/calendar_month.svg';
+import camera from '@/public/icons/camera.svg';
 import check from '@/public/icons/check.svg';
 import checkBox from '@/public/icons/check_box.svg';
 import checkBoxIndeterminate from '@/public/icons/check_box_indeterminate.svg';
@@ -11,6 +13,7 @@ import checkCircleFill from '@/public/icons/check_circle_fill.svg';
 import close from '@/public/icons/close_large.svg';
 import closeSmall from '@/public/icons/close_small.svg';
 import dehaze from '@/public/icons/dehaze.svg';
+import ellipse from '@/public/icons/ellipse.svg';
 import email from '@/public/icons/email.svg';
 import emergencyHome from '@/public/icons/emergency_home.svg';
 import emergencyHomeFill from '@/public/icons/emergency_home_fill.svg';
@@ -23,6 +26,7 @@ import keyboardArrowLeft from '@/public/icons/keyboard_arrow_left.svg';
 import keyboardArrowRight from '@/public/icons/keyboard_arrow_right.svg';
 import keyboardArrowUp from '@/public/icons/keyboard_arrow_up.svg';
 import moreHoriz from '@/public/icons/more_horiz.svg';
+import photo from '@/public/icons/photo.svg';
 import radioButtonChecked from '@/public/icons/radio_button_checked.svg';
 import radioButtonUnchecked from '@/public/icons/radio_button_unchecked.svg';
 import schedule from '@/public/icons/schedule.svg';
@@ -62,4 +66,8 @@ export const Icons = {
   visibilityOff,
   close,
   email,
+  bell,
+  camera,
+  ellipse,
+  photo,
 };
