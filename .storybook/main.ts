bb456537045888fad815a124e207ab10a398f262
JungYeonHwi <wjddusgnl676@naver.com>
2025-11-22 12:06:29 +0900

    refactor: configure Storybook with addons and documentation

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,18 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-a11y',
+    '@storybook/addon-styling-webpack',
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-  typescript: {
-    check: false,
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-    },
-  },
+  staticDirs: ['../public'],
 };
-
 export default config;
