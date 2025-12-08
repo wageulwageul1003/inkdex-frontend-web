80e48c1b41236328edb25d13be31231445620a7b
JungYeonHwi <wjddusgnl676@naver.com>
2025-12-09 00:46:32 +0900

    feat: add new icon assets for UI components including archive, edit, settings, and navigation icons

--- src/components/shared/icons.tsx
+++ src/components/shared/icons.tsx
@@ -1,30 +1,38 @@
+import archive from '@/public/icons/archive.svg';
 import ArrowBackIos from '@/public/icons/arrow_back_ios.svg';
 import arrowLeftAlt from '@/public/icons/arrow_left_alt.svg';
 import arrowRightAlt from '@/public/icons/arrow_right_alt.svg';
+import arrowUpRight from '@/public/icons/arrow_up_right.svg';
+import arrowsSort from '@/public/icons/arrows_sort.svg';
 import asterisk from '@/public/icons/asterisk.svg';
 import bell from '@/public/icons/bell.svg';
 import bookmark from '@/public/icons/bookmark.svg';
 import bookmark_fill from '@/public/icons/bookmark_fill.svg';
 import calendarMonth from '@/public/icons/calendar_month.svg';
 import camera from '@/public/icons/camera.svg';
+import cancel from '@/public/icons/cancel.svg';
 import check from '@/public/icons/check.svg';
 import checkBox from '@/public/icons/check_box.svg';
 import checkBoxIndeterminate from '@/public/icons/check_box_indeterminate.svg';
 import checkBoxOutlineBlank from '@/public/icons/check_box_outline_blank.svg';
 import checkCircleFill from '@/public/icons/check_circle_fill.svg';
+import checkIndeterminateSmall from '@/public/icons/check_indeterminate_small.svg';
 import close from '@/public/icons/close_large.svg';
 import closeSmall from '@/public/icons/close_small.svg';
 import dehaze from '@/public/icons/dehaze.svg';
+import edit from '@/public/icons/edit.svg';
 import ellipse from '@/public/icons/ellipse.svg';
 import email from '@/public/icons/email.svg';
 import emergencyHome from '@/public/icons/emergency_home.svg';
 import emergencyHomeFill from '@/public/icons/emergency_home_fill.svg';
 import error from '@/public/icons/error.svg';
 import errorFill from '@/public/icons/error_fill.svg';
+import gripVertical from '@/public/icons/grip_vertical.svg';
 import heart from '@/public/icons/heart.svg';
 import heart_filled from '@/public/icons/heart_filled.svg';
 import home from '@/public/icons/home.svg';
 import image from '@/public/icons/image.svg';
+import inbox from '@/public/icons/inbox.svg';
 import info from '@/public/icons/info.svg';
 import infoFill from '@/public/icons/info_fill.svg';
 import keyboardArrowDown from '@/public/icons/keyboard_arrow_down.svg';
@@ -32,6 +40,7 @@ import keyboardArrowLeft from '@/public/icons/keyboard_arrow_left.svg';
 import keyboardArrowRight from '@/public/icons/keyboard_arrow_right.svg';
 import keyboardArrowUp from '@/public/icons/keyboard_arrow_up.svg';
 import messageCircle from '@/public/icons/message-circle.svg';
+import moodEmpty from '@/public/icons/mood_empty.svg';
 import moreHoriz from '@/public/icons/more_horiz.svg';
 import note from '@/public/icons/note.svg';
 import person from '@/public/icons/person.svg';
@@ -42,6 +51,7 @@ import radioButtonUnchecked from '@/public/icons/radio_button_unchecked.svg';
 import schedule from '@/public/icons/schedule.svg';
 import search from '@/public/icons/search.svg';
 import send from '@/public/icons/send.svg';
+import settings from '@/public/icons/settings.svg';
 import user from '@/public/icons/user.svg';
 import visibility from '@/public/icons/visibility.svg';
 import visibilityOff from '@/public/icons/visibility_off.svg';
@@ -94,4 +104,14 @@ export const Icons = {
   heart,
   heart_filled,
   messageCircle,
+  archive,
+  arrowUpRight,
+  arrowsSort,
+  cancel,
+  checkIndeterminateSmall,
+  edit,
+  gripVertical,
+  inbox,
+  moodEmpty,
+  settings,
 };
