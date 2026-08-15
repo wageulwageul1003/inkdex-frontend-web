55204cbe54f72b6a791c3413ce94c69140a4c013
JungYeonHwi <wjddusgnl676@naver.com>
2026-08-15 22:46:58 +0900

    refactor: remove summary page components for all, monthly, and yearly views

--- src/app/(auth)/summary/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function Summary() {
-  redirect('/summary/yearly');
-}
--- src/app/(auth)/summary/yearly/page.tsx
+++
@@ -1,5 +0,0 @@
-import { SummaryComponent } from '@/components/views/(protected)/mypage/summary';
-
-export default function Summary() {
-  return <SummaryComponent defaultValue="yearly" />;
-}
